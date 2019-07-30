227ac0f585fb108e3461551661864644d38c8461
Mark Palmer <mark.palmer@itsalllive.com>
2019-07-30 11:29:53 -0400

    added models and set up nugets for my machine

---
+++ backend/JumboCaramelPayroll/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JumboCaramelPayroll.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string SSN { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Email { get; set; }
+
+        public virtual int RoleId { get; set; }
+
+
+    }
+}
---
+++ backend/JumboCaramelPayroll/Models/Role.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JumboCaramelPayroll.Models
+{
+    public class Role
+    {
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        public decimal PayRate { get; set; }
+        public int HourRate { get; set; }
+
+        public virtual List<Employee> Employees { get; set; }
+
+
+
+    }
+}
