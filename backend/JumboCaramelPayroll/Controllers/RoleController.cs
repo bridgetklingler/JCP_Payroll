cef4f1f61d726d28fae938931d7cce54e4ec9979
Mark Palmer <mark.palmer@itsalllive.com>
2019-07-30 13:49:52 -0400

    added role controller and fixed startup

---
+++ backend/JumboCaramelPayroll/Controllers/RoleController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JumboCaramelPayroll.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private SiteContext db;
+
+        public RoleController(SiteContext db)
+        {
+            this.db = db;
+        }
+
+        // GET api/Roles
+        [HttpGet]
+        public ActionResult<IEnumerable<Role>> Get()
+        {
+            return db.Roles;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Role> GetById(int id)
+        {
+            return db.Roles.Single(a => a.RoleId == id);
+        }
+
+
+        // POST api/Role
+        [HttpPost]
+        public ActionResult<IEnumerable<Role>> Post([FromBody] Role artist)
+        {
+            db.Roles.Add(artist);
+            db.SaveChanges();
+            return db.Roles.ToList();
+        }
+
+        // PUT api/values/5
+        [HttpPut]
+        public ActionResult<IEnumerable<Role>> Put([FromBody] Role artist)
+        {
+            db.Roles.Update(artist);
+            db.SaveChanges();
+            return db.Roles.ToList();
+        }
+
+
+        [HttpDelete]
+        public ActionResult<IEnumerable<Role>> Delete(Role artist)
+        {
+            db.Roles.Remove(artist);
+            db.SaveChanges();
+            return db.Roles.ToList();
+        }
+
+    }
+}
--- backend/JumboCaramelPayroll/Startup.cs
+++ backend/JumboCaramelPayroll/Startup.cs
@@ -25,7 +25,15 @@ namespace JumboCaramelPayroll
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddScoped<SiteContext, SiteContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +48,7 @@ namespace JumboCaramelPayroll
                 app.UseHsts();
             }
 
+            app.UseCors("MyPolicy");
             app.UseHttpsRedirection();
             app.UseMvc();
         }
