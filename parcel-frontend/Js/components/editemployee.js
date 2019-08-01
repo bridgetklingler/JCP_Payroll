0db05e50ad918d7e15ed6e3c67e16f612fd82f98
mvernon27 <mvernon2705@gmail.com>
2019-08-01 14:13:29 -0400

    Stylez

--- parcel-frontend/Js/components/editemployee.js
+++ parcel-frontend/Js/components/editemployee.js
@@ -3,15 +3,16 @@ export default function GetEditEmployee(employee){
     return `    
             <h1> Edit Employee</h1>
             <input type="hidden" class="edit_employee_id" value="${employee.employeeId}">
-            <input type="text" class="edit_employee_first_name" value="${employee.firstName}">
-            <input type="text" class="edit_employee_last_name" value="${employee.lastName}">
-            <input type="text" class="edit_employee_address" value="${employee.address}">
-            <input type="text" class="edit_employee_phone_number" value="${employee.phoneNumber}">
-            <input type="text" class="edit_employee_ssn" value="${employee.ssn}">
-            <input type="date" class="edit_employee_birthdate" value="${employee.birthDate}">
-            <p>${employee.birthDate}</p>
-            <input type="text" class="edit_employee_email" value="${employee.email}">
-            <input type="text" class="edit_employee_role" value="${employee.roleId}">
-            <button class="edit_employee_submit multibutton">Submit</button>
+            <addemployee>
+            <addinput> <label>First Name:</label><input type="text" class="edit_employee_first_name" value="${employee.firstName}"></addinput>
+            <addinput> <label>Last Name:</label><input type="text" class="edit_employee_last_name" value="${employee.lastName}"></addinput>
+            <addinput> <label>Address:</label><input type="text" class="edit_employee_address" value="${employee.address}"></addinput>
+            <addinput> <label>Phone Number:</label><input type="text" class="edit_employee_phone_number" value="${employee.phoneNumber}"></addinput>
+            <addinput> <label>SSN:</label><input type="text" class="edit_employee_ssn" value="${employee.ssn}"></addinput>
+            <addinput> <label>BirthDate:</label><input type="date" class="edit_employee_birthdate" value="${employee.birthDate}"></addinput>
+            <addinput> <label>Email:</label><input type="text" class="edit_employee_email" value="${employee.email}"></addinput>
+            <addinput> <label>Role:</label><input type="text" class="edit_employee_role" value="${employee.roleId}"></addinput>
+            <button class="edit_employee_submit multibutton submit">Submit</button>
+            </addemployee>
     `
 }
--- parcel-frontend/Js/components/employeeindex.js
+++ parcel-frontend/Js/components/employeeindex.js
@@ -1,30 +1,44 @@
 export default function EmployeeIndex(employeelist){
     console.log("employee Index")
     return `
-    <employees>  
-        
+    <h1>Employee Index</h1>
+    <employees>
+    <employee style="font-weight: 800; background-color: rgb(120, 161, 182)">    
+    <names>
+            <fname></fname>
+            <lname>Name</lname>
+            </names>
+            <address>Address</address>
+            <pn>Phone Number</pn>
+            <ssn>SSN</ssn>
+            <bd>BirthDate</bd>
+            <email>Email</email>
+            <employeebuttons></employeebuttons>
+        </employee>
         ${employeelist.map(employee => {
         return `  
-                  
-                <button class="edit_employee multibutton">Edit 
-                <input class="employee_id" type="hidden" value="${employee.employeeId}"> 
-                </button> 
-                <button class="delete_employee_submit multibutton">Delete 
-                <input class="delete_employee_id" type="hidden" value="${employee.employeeId}"> 
-                </button> 
-                <button class="single_employee_submit multibutton">Select
-                <input class="single_employee_id" type="hidden" value="${employee.employeeId}"> 
-                </button>
-                
-                     
-                ${employee.firstName}
-                ${employee.lastName}
-                ${employee.address}
-                ${employee.phoneNumber}
-                ${employee.ssn}
-                ${employee.birthdate}
-                ${employee.email}
-
+            <employee>
+            <names>
+            <fname>${employee.firstName}</fname>
+            <lname>${employee.lastName}</lname>
+            </names>
+            <address>${employee.address}</address>
+            <pn>${employee.phoneNumber}</pn>
+            <ssn>${employee.ssn}</ssn>
+            <bd>${employee.birthdate}</bd>
+            <email>${employee.email}</email>
+            <employeebuttons>
+            <button class="edit_employee multibutton">Edit 
+            <input class="employee_id" type="hidden" value="${employee.employeeId}"> 
+            </button> 
+            <button class="delete_employee_submit multibutton">Delete 
+            <input class="delete_employee_id" type="hidden" value="${employee.employeeId}"> 
+            </button> 
+            <button class="single_employee_submit multibutton">Select
+            <input class="single_employee_id" type="hidden" value="${employee.employeeId}"> 
+            </button>
+            </employeebuttons>
+            </employee>
 
                  `      
         })     
