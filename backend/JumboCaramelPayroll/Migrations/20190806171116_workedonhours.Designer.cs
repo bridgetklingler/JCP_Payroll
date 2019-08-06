9b792c3fbf3f79e4d2693cc833a2e35af43589b5
Mark Palmer <mark.palmer@itsalllive.com>
2019-08-06 15:39:36 -0400

    added functionality to hours for calculating hours

--- backend/JumboCaramelPayroll/Migrations/20190806171116_workedonhours.Designer.cs
+++ backend/JumboCaramelPayroll/Migrations/20190806171116_workedonhours.Designer.cs
@@ -4,14 +4,16 @@ using JumboCaramelPayroll;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace JumboCaramelPayroll.Migrations
 {
     [DbContext(typeof(SiteContext))]
-    partial class SiteContextModelSnapshot : ModelSnapshot
+    [Migration("20190806171116_workedonhours")]
+    partial class workedonhours
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
