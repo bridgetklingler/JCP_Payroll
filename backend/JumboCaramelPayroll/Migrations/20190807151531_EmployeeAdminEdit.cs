cfd7da3fec2688b28b635c819a3b0f97092e187d
CLux <cplux7@gmail.com>
2019-08-07 11:19:25 -0400

    added Admin to employee model and updated database with new migration reflecting that info.

---
+++ backend/JumboCaramelPayroll/Migrations/20190807151531_EmployeeAdminEdit.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JumboCaramelPayroll.Migrations
+{
+    public partial class EmployeeAdminEdit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Admin",
+                table: "Employees",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "EmployeeId",
+                keyValue: 1,
+                columns: new[] { "Admin", "Birthdate" },
+                values: new object[] { true, new DateTime(2019, 8, 7, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "EmployeeId",
+                keyValue: 2,
+                column: "Birthdate",
+                value: new DateTime(2019, 8, 7, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "EmployeeId",
+                keyValue: 3,
+                column: "Birthdate",
+                value: new DateTime(2019, 8, 7, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "EmployeeId",
+                keyValue: 4,
+                column: "Birthdate",
+                value: new DateTime(2019, 8, 7, 0, 0, 0, 0, DateTimeKind.Local));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Admin",
+                table: "Employees");
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "EmployeeId",
+                keyValue: 1,
+                column: "Birthdate",
+                value: new DateTime(2019, 8, 6, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "EmployeeId",
+                keyValue: 2,
+                column: "Birthdate",
+                value: new DateTime(2019, 8, 6, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "EmployeeId",
+                keyValue: 3,
+                column: "Birthdate",
+                value: new DateTime(2019, 8, 6, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "EmployeeId",
+                keyValue: 4,
+                column: "Birthdate",
+                value: new DateTime(2019, 8, 6, 0, 0, 0, 0, DateTimeKind.Local));
+        }
+    }
+}
