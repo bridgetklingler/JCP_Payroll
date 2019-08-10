b4460585d820008498a644273b239d47638657d5
wenjen10 <wenjen10@yahoo.com>
2019-08-10 14:39:14 -0400

    formatted edit employee page into table

--- parcel-frontend/Js/components/admin/AdminEditEmployee.js
+++ parcel-frontend/Js/components/admin/AdminEditEmployee.js
@@ -8,24 +8,49 @@ export default function AdminEditEmployee(employee){
          `    
             <h1> Edit: ${employee.lastName}, ${employee.firstName}</h1>
             <input type="hidden" class="edit_employee_id" value="${employee.employeeId}">
-            <h2>Employee ID: ${employee.employeeId}</h2>
-            <addemployee>
-            <addinput> <label>First Name:</label><input type="text" class="edit_employee_first_name" value="${employee.firstName}"></addinput>
-            <addinput> <label>Last Name:</label><input type="text" class="edit_employee_last_name" value="${employee.lastName}"></addinput>
-            <addinput> <label>Address:</label><input type="text" class="edit_employee_address" value="${employee.address}"></addinput>
-            <addinput> <label>Phone Number:</label><input type="text" class="edit_employee_phone_number" value="${employee.phoneNumber}"></addinput>
-            <addinput> <label>SSN:</label><input type="text" class="edit_employee_ssn" value="${employee.ssn}"></addinput>
-            <addinput> <label>BirthDate:</label><input type="date" class="edit_employee_birthdate" value="${employee.birthdate.substring(0,10)}"></addinput>
-            <addinput> <label>Email:</label><input type="text" class="edit_employee_email" value="${employee.email}"></addinput>
-            <div>
-            <addinput> <label>RoleId:</label> <select id="role_select" class="add_employee_roleId"></select></addinput>
-            </div>
+            <h2><em>Employee ID: ${employee.employeeId}</em></h2>
+            
+            <table style = "width: 95%">
+            <tr>
+            <th>First Name:</th>
+            <td><input type="text" class="edit_employee_first_name" value="${employee.firstName}"></td>
+            </tr>
+            
+            <tr>
+            <th>Last Name:</th>
+            <td> <input type="text" class="edit_employee_last_name" value="${employee.lastName}"></td>
+            </tr>
+            <tr>
+             <th>Address:</th>
+             <td><input type="text" class="edit_employee_address" value="${employee.address}"></td>
+            </tr>
+            <tr>
+            <th>Phone Number:</th>
+            <td><input type="text" class="edit_employee_phone_number" value="${employee.phoneNumber}"></td>
+            </tr>
+            <tr>
+            <th>SSN:</th>
+            <td><input type="text" class="edit_employee_ssn" value="${employee.ssn}"></td>
+            </tr>
+            <tr>
+            <th>BirthDate:</th>
+            <td><input type="date" class="edit_employee_birthdate" value="${employee.birthdate.substring(0,10)}"></td>
+            </tr>
+            <tr>
+            <th>Email:</th>
+            <td><input type="text" class="edit_employee_email" value="${employee.email}"></td>
+            </tr><div>
+            <tr>
+            <th>RoleId:</th>
+            <td><select id="role_select" class="add_employee_roleId"></select></td>
+            </tr></div>
+            </table>
             <div class="edit_buttons">
             <button class="edit_employee_submit multibutton submit">Submit</button>
             <button class='cancel_edit_submit multibutton cancel'>Cancel
             </div>
             <input class='cancel_employee_id' type='hidden' value ='${employee.employeeId}'>
-            </addemployee>
+           
     `
     document.querySelector("#main").innerHTML = page;
     
