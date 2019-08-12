e62d227fb9be73bf6e5f04b0086988d3967b1554
Bridget Klingler <bridget.klingler@gmail.com>
2019-08-12 13:30:44 -0400

    code clean up

--- parcel-frontend/Js/components/admin/AdminHoursIndexView.js
+++ parcel-frontend/Js/components/admin/AdminHoursIndexView.js
@@ -6,8 +6,8 @@ export default function AdminHoursIndex(hourslist){
     <p>Select Range</p>
     <input type="datetime-local" class="range_date1">
     <input type="datetime-local" class="range_date2">
-    <h1>Hours Index</h1>
     <button class="getdaterange">Range</button>
+    <h1>Hours Index</h1>
     <hours>
     <hour style="font-weight: 800; background-color: rgb(120, 161, 182)">    
         <dates>
