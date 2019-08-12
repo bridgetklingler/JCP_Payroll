544816328cf5801f83df091fb9851919b827e9ab
Bridget Klingler <bridget.klingler@gmail.com>
2019-08-12 17:54:22 -0400

    Admin Add Hours is in a table and functioning with drop down to select employee

--- parcel-frontend/Js/components/admin/adminaddhours.js
+++ parcel-frontend/Js/components/admin/adminaddhours.js
@@ -8,19 +8,28 @@ console.log("admin add hours")
     function setPage(){
         let page = `    
         <h1> Add Hours</h1>
-        <addemployee>
 
-        <div>
-        <addinput> <label>Employee Id:</label> 
-        <select id="employee_select" class="add_employee_id_hours"></select></addinput> 
-        </div>
-        
-        <addinput> <label>Time In: <label/> <input type="datetime-local" class="add_hours_time_in"> </addinput>
-        <addinput> <label> Time Out: <label/> <input type="datetime-local" class="add_hours_time_out"> </addinput>
-        
-        <addinput> <label> Approved: <label/> <input type="text" class="add_hours_approved" value="false"> </addinput>
+        <table>
+            <tr>
+                <th>Employee Id:</th>
+                <td><select id="employee_select" class="add_employee_id_hours"></select></td>
+            </tr>
+            <tr>
+                <th>Time In:</th>
+                <td><input type="datetime-local" class="add_hours_time_in"></select></td>
+            </tr>
+            <tr>
+                <th>Time Out:</th>
+                <td><input type="datetime-local" class="add_hours_time_out"> </select></td>
+            </tr>
+            <tr>
+                <th>Approved:</th>
+                <td><input type="text" class="add_hours_approved" value="false"></select></td>
+            </tr>
+
+            
+        </table>
         <button class="add_employee_hours_submit multibutton submit">Submit</button>
-        </addemployee>
         `
         document.querySelector('#main').innerHTML = page;
     }
@@ -30,7 +39,6 @@ console.log("admin add hours")
     }
 
     function setEmployees(employees){
-        console.log("admin add hours bottom of page")
         let employeeHtml = '';
         for(const employee of employees){
             employeeHtml += `<option
@@ -38,6 +46,5 @@ console.log("admin add hours")
         }
         document.querySelector("#employee_select").innerHTML = employeeHtml;
     }
-    //console.log(employeeHtml)
 }
 
