16c36a8180aad30c2167bb7c9b1212e9ba7dae5e
Bridget Klingler <bridget.klingler@gmail.com>
2019-08-14 01:11:03 -0400

    changed the uder edit page to a table; added medbutton class to buttons

--- parcel-frontend/Js/components/user/UserEditProfile.js
+++ parcel-frontend/Js/components/user/UserEditProfile.js
@@ -4,21 +4,43 @@ export default function UserEditProfile(employee){
             <h1> Edit: ${employee.lastName}, ${employee.firstName}</h1>
             <input type="hidden" class="edit_employee_id" value="${employee.employeeId}">
             <h2>Employee ID: ${employee.employeeId}</h2>
-            <addemployee>
-            <addinput> <label>First Name:</label><input type="text" class="edit_employee_first_name" value="${employee.firstName}"></addinput>
-            <addinput> <label>Last Name:</label><input type="text" class="edit_employee_last_name" value="${employee.lastName}"></addinput>
-            <addinput> <label>Address:</label><input type="text" class="edit_employee_address" value="${employee.address}"></addinput>
-            <addinput> <label>Phone Number:</label><input type="text" class="edit_employee_phone_number" value="${employee.phoneNumber}"></addinput>
-            <addinput> <label>BirthDate:</label><input type="date" class="edit_employee_birthdate" value="${employee.birthdate.substring(0,10)}"></addinput>
-            <addinput> <label>Email:</label><input type="text" class="edit_employee_email" value="${employee.email}"></addinput>
+
+            <table style="width: 95%">
+                <tr>
+                        <th>First Name: </th>
+                        <td><input type="text" class="edit_employee_first_name" value="${employee.firstName}"></td>
+                </tr>
+                <tr>
+                        <th>Last Name: </th>
+                        <td><input type="text" class="edit_employee_last_name" value="${employee.lastName}"></td>
+                </tr>
+                <tr>
+                        <th>Email: </th>
+                        <td><input type="text" class="edit_employee_email" value="${employee.email}"></td>
+                </tr>
+                <tr>
+                        <th>Phone Number: </th>
+                        <td><input type="text" class="edit_employee_phone_number" value="${employee.phoneNumber}"></td>
+                </tr>
+                <tr>
+                        <th>Address: </th>
+                        <td><input type="text" class="edit_employee_address" value="${employee.address}"></td>
+                </tr>
+                <tr>
+                        <th>Birth Date: </th>
+                        <td><input type="date" class="edit_employee_birthdate" value="${employee.birthdate.substring(0,10)}"></td>
+                </tr>
+
+            </table>
+
             <div>
             <input type="hidden" class="user_roleId" value="${employee.roleId}">
             <input type="hidden" class="user_ssn" value="${employee.ssn}">
             <input type="hidden" class="user_admin_status" value="${employee.admin}">
             </div>
             <div class="edit_buttons">
-            <button class="user_edit_submit multibutton submit">Submit</button>
-            <button class='user_cancel_edit_submit multibutton cancel'>Cancel</button>
+                <button class="user_edit_submit multibutton submit medbutton">Submit</button>
+                <button class='user_cancel_edit_submit multibutton cancel medbutton'>Cancel</button>
             </div>
 
             <input class='cancel_employee_id' type='hidden' value ='${employee.employeeId}'>
